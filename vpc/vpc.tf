41eea4de17829eec3f783f5890aac28f89750aa8
devarshi-balu <studious.devarshibalu@gmail.com>
2026-07-02 11:48:13 +0530

    fix comments

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -28,21 +28,21 @@ resource "aws_internet_gateway" "main"{
     })
 }
 
-# resource "aws_eip" "nat_ip" {
-#     domain = "vpc"
-#     tags = merge(local.common_tags, {
-#         Name = "${local.combined_name}-nat_ip"
-#     })
-# }
+resource "aws_eip" "nat_ip" {
+    domain = "vpc"
+    tags = merge(local.common_tags, {
+        Name = "${local.combined_name}-nat_ip"
+    })
+}
 
-# resource "aws_nat_gateway" "main" {
-#     allocation_id = aws_eip.nat_ip.id
-#     subnet_id = aws_subnet.main[keys(local.public_subnets)[0]].id
+resource "aws_nat_gateway" "main" {
+    allocation_id = aws_eip.nat_ip.id
+    subnet_id = aws_subnet.main[keys(local.public_subnets)[0]].id
     
-#     tags = merge(local.common_tags, {
-#         Name = "${local.combined_name}-main_nat"
-#     })
-# }
+    tags = merge(local.common_tags, {
+        Name = "${local.combined_name}-main_nat"
+    })
+}
 
 resource "aws_route_table" "public"{
     vpc_id = aws_vpc.main.id
@@ -70,10 +70,10 @@ resource "aws_route_table" "public"{
 resource "aws_route_table" "private"{
     vpc_id = aws_vpc.main.id
 
-    # route {
-    #     cidr_block = "0.0.0.0/0"
-    #     nat_gateway_id = aws_nat_gateway.main.id
-    # }
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.main.id
+    }
 
     route {
         cidr_block = data.aws_vpc.default.cidr_block
